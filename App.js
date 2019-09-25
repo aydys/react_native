35a488a57e48fcb36a9cdbe5398488d322337daf
Aidys Dongak <aidongak23@gmail.com>
2019-09-25 14:10:29 +0700

    my first commit

---
+++ App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      people: []
+    };
+  }
+  async componentDidMount() {
+    const res = await fetch('https://swapi.co/api/people');
+    const { results } = await res.json();
+    this.setState(() => {
+      return {
+        people: results
+      };
+    });
+  }
+  render() {
+    return (
+      <View styles={styles.container}>
+        <Text style={{ color: '#fcd433', fontSize: 40, padding: 10 }}>
+          A long time ago, in a Galaxy far, far away...
+        </Text>
+        <Text>Here are some cool people:</Text>
+        {this.state.people.map(p => {
+          return (
+            <Text style={{ color: '#fcd433'}} key={p.name}>
+              {p.name}
+            </Text>
+          )
+        })}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+});
